ee936c6977ac58ab5d07c09d54ca84aa0c4ea67b
unmakame <bayanovlad@gmail.com>
2025-09-29 18:54:26 +0300

    last fix и статические методы отличаются от обычных методов тем что они работают со статическими членами класса и могут вызываться без экземпляра класса например как у меня в коде int Binary::compare(const Binary& a, const Binary& b); а обычные методы имеют привязку к элементам класса точнее к экземплярам

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vladislavbaanov/Desktop/OOP_Laba_2
+# Build directory: /Users/vladislavbaanov/Desktop/OOP_Laba_2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(BinaryTests "/Users/vladislavbaanov/Desktop/OOP_Laba_2/build/binary_tests")
+set_tests_properties(BinaryTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/vladislavbaanov/Desktop/OOP_Laba_2/CMakeLists.txt;30;add_test;/Users/vladislavbaanov/Desktop/OOP_Laba_2/CMakeLists.txt;0;")
